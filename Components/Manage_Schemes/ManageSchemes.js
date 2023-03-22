c03feebacbd80f2a042a0df83b44c1b4d4450c61
Yusuf Ahmed <108522419+yusuf-9@users.noreply.github.com>
2023-03-22 15:56:01 +0530

    No comments

--- Components/Manage_Schemes/ManageSchemes.js
+++ Components/Manage_Schemes/ManageSchemes.js
@@ -47,27 +47,6 @@ export default function ManageScheme({ schemes }) {
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
-                            <tbody>
-
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
                         </table>
                     </div>
 
