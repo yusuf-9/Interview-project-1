95112470001ae33b7d4cdba80a9c5f5d9be2c164
Yusuf Ahmed <108522419+yusuf-9@users.noreply.github.com>
2023-03-22 16:13:17 +0530

    final commit

--- pages/dashboard/manage_scheme.js
+++ pages/dashboard/manage_scheme.js
@@ -1,6 +1,5 @@
 import DashboardNavbar from "@/Components/Dashboard_Navbar/DashboardNavbar";
 import ManageScheme from "@/Components/Manage_Schemes/ManageSchemes";
-import { db } from "@/lib/db";
 
 
 export default function ManageSchemes(){
